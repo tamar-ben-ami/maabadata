8935aa3f191a8182d4dfafa3f7bc7787bd9f6388
tamar-ben-ami <59728676+tamar-ben-ami@users.noreply.github.com>
2022-11-30 18:20:28 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+
+LABEL_FIELD = "STAT_CAUSE_CODE"
+
+def date_features(df):
+    df["cont_date_dt"] = pd.to_datetime(df.CONT_DATE, origin="julian",
+                                        unit='D')
+    df["disc_date_dt"] = pd.to_datetime(df.DISCOVERY_DATE, origin="julian",
+                                        unit='D')
+    df["cont_year"] = df["cont_date_dt"].apply(
+        lambda x: 0 if np.isnan(x.year) else int(x.year))
+    df["cont_mon"] = df["cont_date_dt"].apply(
+        lambda x: 0 if np.isnan(x.month) else int(x.month))
+    df["cont_dow"] = df["cont_date_dt"].apply(
+        lambda x: 0 if np.isnan(x.weekday()) else int(x.weekday()))
+    df["cont_is_weekend"] = df["cont_dow"].isin([6, 5, 4]).astype(int)
+    df["disc_year"] = df["disc_date_dt"].apply(
+        lambda x: 0 if np.isnan(x.year) else int(x.year))
+    df["disc_mon"] = df["disc_date_dt"].apply(
+        lambda x: 0 if np.isnan(x.month) else int(x.month))
+    df["disc_dow"] = df["disc_date_dt"].apply(
+        lambda x: 0 if np.isnan(x.weekday()) else int(x.weekday()))
+    df["disc_is_weekend"] = df["disc_dow"].isin([6, 5, 4]).astype(int)
+
+    df["time_to_cont"] = df["cont_date_dt"] - df["disc_date_dt"]
+    df["time_to_cont"] = df["time_to_cont"].apply(
+        lambda x: 0 if pd.isnull(x) else x.days)
+
+    features = ["cont_year", "cont_mon", "cont_dow", "cont_is_weekend",
+                "disc_year", "disc_mon", "disc_dow", "disc_is_weekend",
+                "time_to_cont"]
+
+    return features
